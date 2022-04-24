3c665a0329581cc1b1b6079e135d298da8c5a7ac
victufell <victor.fellype.a@gmail.com>
2022-04-23 21:53:15 -0300

    added Navigation tabs

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
@@ -6,13 +6,38 @@ import { StyleSheet } from 'react-native';
 import Home from './src/screens/Home'
 import Login from './src/screens/Login'
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
+
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
+  return (
+    <Home />
+  );
+
+}
+function LoginScreen() {
+  return (
+    <Login />
+  );
+}
+
 export default function App() {
 
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <Login />
-    </SafeAreaView>
+    <React.Fragment>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+            <Tab.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+          </Tab.Navigator>
+        </NavigationContainer> 
+      </SafeAreaProvider>
+    </React.Fragment>
   );
 }
 
