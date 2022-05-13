396014f30c39866580af1c4dcdc01c50f2d564ce
BrenoLuizetto <cintrabreno22@gmail.com>
2022-05-12 21:44:45 -0300

    adicionada navegacao

--- App.js
+++ App.js
@@ -1,51 +1,47 @@
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
-
-import Home from './src/screens/Home'
-import Login from './src/screens/Login'
+import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import React from 'react';
 
-const Tab = createBottomTabNavigator();
+import Home from './src/screens/Home';
+import Login from './src/screens/Login';
+import Register from './src/screens/Register';
 
-function HomeScreen() {
-  return (
-    <Home />
-  );
+const Stack = createNativeStackNavigator();
 
+export default function App(){
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen 
+
+        name="Login" 
+        component={Login} 
+        options={{
+          title: 'Login',
+          headerStyle:{
+            backgroundColor: '#4287f5'
+          },
+
+          headerTintColor: '#fff',
+          headerShown: true,
+        }}
+        />
+
+        <Stack.Screen 
+        name="Home" 
+        component={Home} 
+       
+        />
+
+        
+      <Stack.Screen
+        name="Register"
+        component={Register}
+      />
+
+    
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-function LoginScreen() {
-  return (
-    <Login />
-  );
-}
-
-export default function App() {
-
-  return (
-    <React.Fragment>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-            <Tab.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-          </Tab.Navigator>
-        </NavigationContainer> 
-      </SafeAreaProvider>
-    </React.Fragment>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
